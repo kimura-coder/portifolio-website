c083ef04f2321a0d792bf92407f4920af7dc83ec
kimura-coder <67652356+kimura-coder@users.noreply.github.com>
2023-12-03 12:43:50 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,13 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Alfred Mwangi Kimura",
+  title: "Web Developer & Cybersecurity Expert ",
+  email: "kimuraalfred@gmail.com",
+  gitHub: "https://github.com/kimura-coder",
+  instagram: "https://www.instagram.com/alfumrasta",
+  linkedIn: "https://www.linkedin.com/in/alfred-kimura-2a20b6230/",
+  twitter: "https://twitter.com/MwangiKimura",
 };
 
 const primaryColor = "#4E567E";
